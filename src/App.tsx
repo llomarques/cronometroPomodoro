6d6964db2ac71c393aae7068f5dd755326b9c219
llomarques <dev@DSN1191348087.edu119.local>
2026-03-11 11:18:52 -0300

    meu aniversario

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Heading} from './components/heading';
 import {Container} from './components/container'
 import {Logo} from './components/logo'
 import {Menu} from './components/menu'
+import { CountDown } from './components/countdown';
 export function App() {
   return (
     <>
@@ -14,6 +15,26 @@ export function App() {
     <Container>
       <Menu/>
     </Container>
+
+    <Container>
+      <CountDown/> 
+      </Container>
+      <Container>
+        <form action="">
+          <div>
+            <label htmlFor="">task</label>
+            <input id = "meuInput"type = "text" />
+          </div>
+          <div>
+            linha verde 
+          </div>
+          <div>
+            <p>ciclos</p>
+          </div>
+          <button> enviar</button>
+        </form>
+      </Container>
+
 
       <Container>
         <Heading>Logo</Heading>
